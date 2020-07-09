cf37e610d4e28172738a4d2684e3d77a09f2f8a0
ArunTeltia <teltia.arun18@gmail.com>
2020-07-10 02:28:28 +0530

    contact us form nodemailer

--- server.js
+++ server.js
@@ -2,8 +2,92 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+const { check, validationResult } = require("express-validator");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
-app.listen(3000,()=>{
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.get('/contact',(req,res)=>{
+    res.render("contact",{
+        message:""
+    })
+});
+
+app.post('/sendmessage', [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("message", "Message is required").not().isEmpty(),
+
+],
+
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors);
+            res.render("contact",{
+                message:"Fill"
+            });
+        }
+        const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+        // create reusable transporter object using the default SMTP transport
+        const transporter = nodemailer.createTransport({
+            service: 'outlook',
+            port: 587,
+            auth: {
+                user: 'sfoly@outlook.com',
+                pass: '@Springfield18@'
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: "sfoly@outlook.com", // sender address
+            to: 'teltia.arun18@gmail.com', // list of receivers
+            subject: 'Node Contact Request', // Subject line
+            text: 'Hello world?', // plain text body
+            html: output // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            console.log(info);
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            res.render('contact',{
+                message:"Message"
+            });
+        });
+    });
+
+
+app.listen(3000, () => {
     console.log('server is running');
 })
